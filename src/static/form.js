7c426ecb7f6c5093e154bf10d8e186b1241821d4
Sauli Kylmänen <62215427+saukkko@users.noreply.github.com>
2022-04-17 17:33:31 +0300

    added styling to front end and buttons for each route

--- src/static/form.js
+++ src/static/form.js
@@ -1,21 +1,85 @@
 window.onload = () => {
   const form = document.getElementById("form");
-  const button = document.getElementById("button");
+  const getAllButton = document.getElementById("getall");
+  const getButton = document.getElementById("get");
+  const addButton = document.getElementById("add");
+  const updateButton = document.getElementById("update");
+  const deleteButton = document.getElementById("delete");
 
-  button.addEventListener("click", () => {
-    const payload = {
-      name: form.name.value,
-      user_id: form.user_id.value,
-      dob: Date.parse(form.dob.value),
-    };
+  /**
+   * Get all data
+   */
+  getAllButton.addEventListener("click", () => {
+    sendRequest("/api/getall", "GET", null);
+  });
 
-    fetch("/api/add", {
-      method: "post",
-      body: JSON.stringify(payload),
+  /**
+   * Get data
+   */
+  getButton.addEventListener("click", () => {
+    const formData = readForm(form);
+    sendRequest(`/api/${formData.id}`, "GET", null);
+  });
+
+  /**
+   * Add new data
+   */
+  addButton.addEventListener("click", () => {
+    const formData = readForm(form);
+    sendRequest("/api/add", "POST", JSON.stringify(formData));
+  });
+
+  /**
+   * Update existing data
+   */
+  updateButton.addEventListener("click", () => {
+    const formData = readForm(form);
+    sendRequest(
+      `/api/update/${formData.id}`,
+      "PATCH",
+      JSON.stringify(formData)
+    );
+  });
+
+  /**
+   * Delete data
+   */
+  deleteButton.addEventListener("click", () => {
+    const formData = readForm(form);
+    sendRequest(`/api/delete/${formData.id}`, "DELETE", null);
+  });
+
+  /* Helper functions */
+  const sendRequest = (endpoint, method, payload) => {
+    fetch(endpoint, {
+      method: method,
+      body: payload,
       headers: { "Content-Type": "application/json;charset=utf-8" },
     })
       .catch(console.error)
-      .then((res) => res.json())
-      .then(console.log);
-  });
+      .then((res) => res.text())
+      .then((data) => {
+        console.log(data);
+        try {
+          const response = JSON.parse(data);
+          displayResults(response);
+        } catch (err) {
+          if (err instanceof SyntaxError && err.message.includes("JSON"))
+            console.log(err);
+        }
+      });
+  };
+
+  const readForm = (form) => {
+    return { name: form.name.value, id: parseInt(form.id.value) };
+  };
+
+  const displayResults = (data) => {
+    console.log(data);
+    document.getElementById("response").innerText = JSON.stringify(
+      data,
+      null,
+      2
+    );
+  };
 };
